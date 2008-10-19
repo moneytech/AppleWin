3e606297aeb3fd8f859d765aa0915ac687206065
tomch <tomch@b5e51362-a710-0410-a134-be2176fc2e42>
2008-10-19 19:59:35 +0000

    Integrated FUSE's AY8910 emu module into AppleWin (Only 1x AY support)

--- AppleWin/source/AY8910.h
+++ AppleWin/source/AY8910.h
@@ -1,12 +1,14 @@
 #ifndef AY8910_H
 #define AY8910_H
 
+#define DBG_SINGLE_AY8910_CHIP
+
 #define MAX_8910 4
 
 void _AYWriteReg(int n, int r, int v);
-void AY8910_write_ym(int chip, int addr, int data);
+//void AY8910_write_ym(int chip, int addr, int data);
 void AY8910_reset(int chip);
-void AY8910Update(int chip,INT16 **buffer,int length);
+void AY8910Update(int chip, INT16** buffer, int nNumSamples);
 
 void AY8910_InitAll(int nClock, int nSampleRate);
 void AY8910_InitClock(int nClock);
